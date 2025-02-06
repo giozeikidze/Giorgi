6fef2c345bc1c622c48a477dc67545ec2cbbd65f
giozeikidze <79111127+giozeikidze@users.noreply.github.com>
2025-02-06 13:20:40 -0500

    work

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
 #!/usr/bin/env node
 
-console.log("Giorgi");
+const myName = "Giorgi";
+
+let myDay = "My birthday day"; 
+myDay = "Saturday"; 
+const message = `Greetings! I am ${myName} and I love ${myDay} this year!`
+
+console.log(message);
